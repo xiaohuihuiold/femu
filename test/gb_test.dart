fc0e2c8a231c508ce3679f265b955024e00726ae
xiaohuihuiold <xiaohuihuiold@gmail.com>
2024-05-23 00:00:56 +0800

    test(gb_test): GBRomLoader测试

--- test/gb_test.dart
+++ test/gb_test.dart
@@ -1,10 +1,14 @@
+import 'package:femu/src/emulators/gb/gb_rom_loader.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:femu/gb.dart';
 
 void main() {
   test(
     'GB test',
-    () async {},
+    () async {
+      final rom = await GBRomLoader.loadFromPath('./demos/gb/test.gb');
+      print(rom);
+    },
     timeout: const Timeout(Duration(days: 1)),
   );
 }
